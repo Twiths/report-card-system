478302ac3316f812309723b3b3ed78a840ab8306
Kally <kallyasmedia@gmail.com>
2021-01-13 07:31:57 +0000

    Add modify function

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ FILE *fptr;
 void write_stdntudent() {
   fptr = fopen("student.dat", "ab");
   printf("\nPlease Enter The New Details of stdntudent \n");
-  printf("\nEnter The roll number of stdntudent ");
+  printf("\nEnter The Reg Number of stdntudent ");
   scanf("%d", &stdnt.regNumber);
   fflush(stdin);
   printf("\n\nEnter The Name of student ");
@@ -53,7 +53,7 @@ void write_stdntudent() {
 }
 
 void printInfo() {
-  printf("\nRoll Number of Student : %d", stdnt.regNumber);
+  printf("\nReg Number of Student : %d", stdnt.regNumber);
   printf("\nName of student : %s", stdnt.name);
   printf("\nMarks in Physics : %d", stdnt.p_marks);
   printf("\nMarks in Chemistry : %d", stdnt.c_marks);
@@ -90,5 +90,62 @@ void display_sp(int n) {
   fclose(fptr);
   if (flag == 0)
     printf("\n\nrecord not exist");
+  getch();
+}
+
+void modify_student() {
+  int no, found = 0;
+  system("clear");
+  printf("\n\n\tTo Modify ");
+  printf("\n\n\tPlease Enter The Reg Number of student");
+  scanf("%d", &no);
+  fptr = fopen("student.dat", "rb+");
+  while ((fread(&stdnt, sizeof(stdnt), 1, fptr)) > 0 && found == 0) {
+    if (stdnt.regNumber == no) {
+      printf("\nReg Number of student : %d", stdnt.regNumber);
+      printf("\nName of student : %s", stdnt.name);
+      printf("\nMarks in Physics : %d", stdnt.p_marks);
+      printf("\nMarks in Chemistry : %d", stdnt.c_marks);
+      printf("\nMarks in Maths : %d", stdnt.m_marks);
+      printf("\nMarks in English : %d", stdnt.e_marks);
+      printf("\nMarks in Computer Science : %d", stdnt.cs_marks);
+      printf("\nPercentage of student is  : %.2f", stdnt.per);
+      printf("\nGrade of student is : %c", stdnt.grade);
+      printf("\nPlease Enter The New Details of student \n");
+      printf("\nEnter The Reg Number of student ");
+      scanf("%d", &stdnt.regNumber);
+      fflush(stdin);
+      printf("\n\nEnter The Name of student ");
+      gets(stdnt.name);
+      printf("\nEnter The marks in physics out of 100 : ");
+      scanf("%d", &stdnt.p_marks);
+      printf("\nEnter The marks in chemistry out of 100 : ");
+      scanf("%d", &stdnt.c_marks);
+      printf("\nEnter The marks in maths out of 100 : ");
+      scanf("%d", &stdnt.m_marks);
+      printf("\nEnter The marks in english out of 100 : ");
+      scanf("%d", &stdnt.e_marks);
+      printf("\nEnter The marks in computer science out of 100 : ");
+      scanf("%d", &stdnt.cs_marks);
+      stdnt.per = (stdnt.p_marks + stdnt.c_marks + stdnt.m_marks +
+                   stdnt.e_marks + stdnt.cs_marks) /
+                  5.0;
+      if (stdnt.per >= 60)
+        stdnt.grade = 'A';
+      else if (stdnt.per >= 50 && stdnt.per < 60)
+        stdnt.grade = 'B';
+      else if (stdnt.per >= 33 && stdnt.per < 50)
+        stdnt.grade = 'C';
+      else
+        stdnt.grade = 'F';
+      fseek(fptr, -(long)sizeof(stdnt), 1);
+      fwrite(&stdnt, sizeof(stdnt), 1, fptr);
+      printf("\n\n\t Record Updated");
+      found = 1;
+    }
+  }
+  fclose(fptr);
+  if (found == 0)
+    printf("\n\n Record Not Found ");
   getch();
 }
